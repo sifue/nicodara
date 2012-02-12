1e97e4c19eaf73203f285aaf3fe5928dcfa3ef07
Soichiro Yoshimura <yoshimura@soichiro.org>
2012-02-12 15:36:11 +0900

    ON/OFF function was implimented.

--- nicodara.js
+++ nicodara.js
@@ -1,18 +1,15 @@
 // Copyright 2012- Soichiro Yoshimura All Rights Reserved.
 /**
- * ニコニコ生放送用のコンテントスクリプト
- *
- * 現在生放送で見られるかどうかを監視し、
- * そうで、ない場合は他の生放送で見られる番組にジャンプさせる
+ * Content script for niconico live continuous watching.
  * @fileoverview contents script of chrome extentison.
  * @author yoshimura@soichiro.org (Soichiro Yoshimura)
  */
 (function()
  {
    /**
-    * ニコニコ生放送のURLからIDを取得する
-    * * @param {string} ニコニコ生放送のURL
-    * * @return {string} ニコニコ生放送のID
+    * get niconico live id 
+    * @param {string} URL of niconico live
+    * @return {string} ID of niconico live
     */
    var getId = function(url){
      url.match(/^http:\/\/live\.nicovideo\.jp\/watch\/([a-z]+[0-9]+)($|\?.*$)/);
@@ -21,11 +18,10 @@
    };
 
    /**
-    * APIの戻り値のxmlを元に、生放送で現在見られるかを返す
-    * @param {Document} http://live.nicovideo.jp/api/getplayerstatus 
-    *                   の結果のXML
-    * @return {boolean} 生放送で見られるかどうかを返す
-    */
+    * get abeilability of live watchning of this video.
+    * @param {Document} xml of http://live.nicovideo.jp/api/getplayerstatus 
+    * @return {boolean} aveilabilty of live watching.
+    **/
    var canLiveWatch = function(data){
      //console.dir(data);
      var endTimeElem = $(data).find('end_time');
@@ -42,20 +38,8 @@
          }
    };
 
-   /////// start main
-
-   var url = document.URL;
-   var id = getId(url);
-
-   var statusURL = 'http://live.nicovideo.jp/api/getplayerstatus';
-   $.get(statusURL, {v:id}, function(data){
-     var isLiveWatch = canLiveWatch(data);
-     console.log('isLiveWatch:' + isLiveWatch);
-     if(!isLiveWatch) showNextVideo();
-   });
-
    /**
-    * 次のビデオを表示する
+    * show next live video.
     */
    var showNextVideo = function(){
      var rssURL = 'http://live.nicovideo.jp/rss';
@@ -67,16 +51,50 @@
          var startTime = new Date(startTimeStr);
          var diffMsecFromNow =  new Date().getTime() - startTime.getTime();
          // console.log('diffMsecFromNow:' + diffMsecFromNow);
-         // 始まって60分以内のものだけ抽出
+         // filter by start time.
          return 0 < diffMsecFromNow && diffMsecFromNow < (60 * 60 * 1000);
        });
 
        console.log('liveItems.length:'+liveItems.length);
-       if(liveItems.length != 0){
-         var nextURL = liveItems.find('link').first().text();
-         console.log(nextURL);
+       var nextURL = null;
+       if(liveItems.length != 0){ 
+         nextURL = liveItems.find('link').first().text();
        }
+
+       console.log('nextURL:'+ nextURL);
+       if(nextURL == null) return;
+
+       return; // TODO clear this code.
+       $('body').animate({"opacity":0}, 1000, function(){
+         location.href = nextURL;
+       });
+
      });
    };
 
+   /**
+    * observe live video status.
+    */
+   var observe = function(){
+     // get status of activation from background.html
+     chrome.extension.sendRequest({field: 'isActive'}, function(response) {
+       var isActive =  response.isActive;
+       console.log('isActive:' + isActive);
+       var url = document.URL;
+       var id = getId(url);
+
+       var statusURL = 'http://live.nicovideo.jp/api/getplayerstatus';
+       $.get(statusURL, {v:id}, function(data){
+         var isLiveWatch = canLiveWatch(data);
+         console.log('isLiveWatch:' + isLiveWatch);
+         if(!isLiveWatch && isActive) {  
+           showNextVideo();
+         }
+         setTimeout(observe,5000); // wait and retry
+       });
+     });
+   }
+
+   /////// start main
+   observe();
  })();
