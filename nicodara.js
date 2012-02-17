e0e6ed4b88f7da6196c90c39ee62f8da57b18857
Soichiro Yoshimura <yoshimura@soichiro.org>
2012-02-17 18:33:36 +0900

    implement search

--- nicodara.js
+++ nicodara.js
@@ -18,11 +18,22 @@
    };
 
    /**
+    * get niconico live id from search url
+    * @param {string} URL of niconico live search result
+    * @return {string} ID of niconico live
+    */
+   var getIdFromSearch = function(url){
+     url.match(/^http:\/\/live\.nicovideo\.jp\/searchresult\?v=([a-z]+[0-9]+)(&.*$)/);
+     var id = RegExp.$1;
+     return id;
+   };
+
+   /**
     * get abeilability of live watchning of this video.
     * @param {Document} xml of http://live.nicovideo.jp/api/getplayerstatus 
     * @return {boolean} aveilabilty of live watching.
     **/
-   var canLiveWatch = function(data){
+   var isLiveWatchNow = function(data){
      var endTimeElem = $(data).find('end_time');
      var endTime = endTimeElem.length === 0 
        ? null 
@@ -37,21 +48,19 @@
    };
 
    /**
-    * show next live video.
+    * show next official live video.
+    * @param {object} status object of background.html
     */
-   var showNextVideo = function(){
+   var showNextOfficial = function(st){
      var rssURL = 'http://live.nicovideo.jp/rss';
      $.get(rssURL, function(rss){
 
-       var ngWords = [
-       'ネットチケットが必要',
-       'この番組は有料'
-       ];
+       var ngWords = st.ngWords.split(' ');
        var items = $(rss).find("item").filter(function(index){
          var text =  $(this).text();
          // filter by NG words
          for(i = 0; i < ngWords.length ; i++){
-          if(text.indexOf(ngWords[i]) > 0) return false;
+           if(text.indexOf(ngWords[i]) > 0) return false;
          }
          return true;
        });
@@ -64,21 +73,57 @@
          return 0 < diffMsecFromNow && diffMsecFromNow < (60 * 60 * 1000);
        });
 
-       // console.dir(items);
        var nextURL = null;
        if(liveItems.length != 0){ 
          nextURL = liveItems.find('link').first().text();
        }else{
          nextURL = items.find('link').first().text();
        }
+       showNextURL(nextURL);
+     });
+   };
+
+
+   /**
+    * show next keyword searchd video.
+    * @param {object} status object of background.html
+    */
+   var showNextKeywordSearched = function(st){
+     var encodedKeyword = encodeURI(st.keyword);
+     var searchURL = 'http://live.nicovideo.jp/search/?orig_filter=+%3Ahidecomonly%3A&sort=point&date=&keyword='
+       + encodedKeyword + 
+      '&submit.x=0&submit.y=0';
+
+     $.get(searchURL, function(data){
+      var firstItem = $(data).find('.search_stream_title a').first();
+      if(firstItem.length === 0) return;
+      var nextURL = firstItem.attr('href');
+      showNextURL(nextURL);
+     });
+   };
+
+   /**
+    * show next channnel id video.
+    * @param {object} status object of background.html
+    */
+   var showNextChannelIdSelected = function(st){
+
+   };
+   
+   /**
+    * show next url with animation.
+    * if next url is same or same channel id, don't show.
+    * @param {string} next url
+    */
+   var showNextURL = function(nextURL){
        // console.log('nextURL:'+ nextURL);
-       if(nextURL == null || nextURL == document.URL) return;
+       if(nextURL === null 
+           || nextURL === document.URL
+           || getIdFromSearch(nextURL) === getId(document.URL)) return;
 
        $('body').animate({"opacity":0}, 1000, function(){
-        location.href = nextURL;
+         location.href = nextURL;
        });
-
-     });
    };
 
    /**
@@ -93,25 +138,39 @@
    }; 
 
    var isFirstObserve = true;
+   var isLiveWatchLast = true;
 
    /**
     * observe live video status.
     */
    var observe = function(){
      // get status of activation from background.html
-     chrome.extension.sendRequest({field: 'isActive'}, function(response) {
-       var isActive =  response.isActive;
+     chrome.extension.sendRequest({field: 'st'}, function(response) {
+       var st = response.st;
        var id = getId(document.URL);
-       if(id == null) return;
+       if(id === null || id === '') return;
 
        var statusURL = 'http://live.nicovideo.jp/api/getplayerstatus';
        $.get(statusURL, {v:id}, function(data){
-         var isLiveWatch = canLiveWatch(data);
-         if(!isLiveWatch && isActive) {  
-           showNextVideo();
+         var isLiveWatch = isLiveWatchNow(data);
+
+         // If change "not live" to "live", execute reload.
+         if(!isLiveWatchLast && isLiveWatch) location.reload(true);
+         isLiveWatchLast = isLiveWatch;
+
+         // show next channel
+         if(!isLiveWatch && st.isActive) {  
+           if(st.searchType === 'official'){
+             showNextOfficial(st);
+           }else if (st.searchType === 'keyword'){
+             showNextKeywordSearched(st);
+           }else if (st.searchType === 'channel_id'){
+             showNextChannelIdSelected(st);
+           }
          }
-         // first observe
-         if(isFirstObserve && isActive){
+
+         // if first observe, show flashplayer
+         if(isFirstObserve && st.isActive){
            showFlashPlayer();
            isFirstObserve = false;
          }
